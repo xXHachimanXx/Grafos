dac1f01559e2bc249c7ee6fecdb4bf3b540658c6
xXHachimanXx <globaldep90@gmail.com>
2019-10-01 21:43:47 -0300

    Adicionando headerfile Grafo.h .

---
+++ includes/Grafo.h
@@ -0,0 +1,14 @@
+using namespace std;
+
+class Grafo
+{
+    public:
+        int numVertices; //numero de vertices
+        int ** matriz; //matriz de adjascência        
+
+        ~Grafo(); //destrutor
+        Grafo(int tam); //construtor   
+        void init(); //inicializador
+        void init(int tam); //inicializador
+                
+};
